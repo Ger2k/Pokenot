edf81f8fe6252b619d84dc7b14c378524944a45f
Ger <musikadicto@gmail.com>
2023-06-14 12:19:48 +0200

    Tutorial completado menos sonido

---
+++ data/attacks.js
@@ -0,0 +1,26 @@
+const attacks = {
+	Tackle: {
+		name: "Tackle",
+		damage: 10,
+		type: "Normal",
+		color: "black"
+	},
+	Fireball: {
+		name: "Fireball",
+		damage: 100,
+		type: "Fire",
+		color: "red"
+	},
+	Surf: {
+		name: "Surf",
+		damage: 25,
+		type: "Water",
+		color: "blue"
+	},
+	Bubbles: {
+		name: "Bubbles",
+		damage: 20,
+		type: "Water",
+		color: "blue"
+	}
+}
